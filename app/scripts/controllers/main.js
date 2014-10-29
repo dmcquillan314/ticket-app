1cbf9097ec302a6ba5ed8fd0597a3dd3ee80ef8e
Daniel McQuillan <dmcquillan@ahold.com>
2014-10-29 00:08:49 -0500

    Set up ticket view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -25,23 +25,7 @@ angular.module('ticketApp')
             });
         };
 
-        $scope.createAccount = function(email, pass, confirm) {
-            $scope.err = null;
-            if( !pass ) {
-                $scope.err = 'Please enter a password';
-            }
-            else if( pass !== confirm ) {
-                $scope.err = 'Passwords do not match';
-            }
-            else {
-                simpleLogin.createAccount(email, pass/*, name*/)
-                    .then(function() {
-                        $location.path('/account');
-                    }, function(err) {
-                        $scope.err = err;
-                    });
-            }
-        };
+
 
         function login(provider, opts) {
             $scope.err = null;
--- app/scripts/controllers/ticket.js
+++ app/scripts/controllers/ticket.js
@@ -1,7 +1,25 @@
 angular.module('ticketApp')
 
-.controller('TicketCtrl', [ '$scope', 'user', function($scope, user) {
+.controller('TicketCtrl', [ '$scope', '$location', 'user', 'simpleLogin', function($scope, $location, user, simpleLogin) {
     'use strict';
 
-    void($scope, user);
+    void(user);
+
+    $scope.createAccount = function(email, pass, confirm) {
+        $scope.err = null;
+        if( !pass ) {
+            $scope.err = 'Please enter a password';
+        }
+        else if( pass !== confirm ) {
+            $scope.err = 'Passwords do not match';
+        }
+        else {
+            simpleLogin.createAccount(email, pass/*, name*/)
+                .then(function() {
+                    $location.path('/account');
+                }, function(err) {
+                    $scope.err = err;
+                });
+        }
+    };
 }]);
